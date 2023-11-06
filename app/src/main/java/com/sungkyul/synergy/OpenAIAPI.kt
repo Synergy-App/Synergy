8f29b50120e97d2f8bc8d5652f9e4d0f77905e21
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2023-11-06 18:36:19 +0900

    [feat] OpenAIAPI.kt 추가

---
+++ app/src/main/java/com/sungkyul/synergy/OpenAIAPI.kt
@@ -0,0 +1,67 @@
+package com.sungkyul.synergy
+
+import com.aallam.openai.api.chat.ChatCompletion
+import com.aallam.openai.api.chat.ChatCompletionRequest
+import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.client.OpenAI
+import com.aallam.openai.api.http.Timeout
+import com.aallam.openai.api.model.ModelId
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+/*
+    [Requirements]
+    1. 'build.gradle.kts'에 추가
+        implementation("com.aallam.openai:openai-client:3.5.0")
+        implementation("io.ktor:ktor-client-android:2.3.5")
+    2. 'AndroidManifest.xml'에 추가
+        <uses-permission android:name="android.permission.INTERNET" />
+
+    [References]
+    - https://github.com/Aallam/openai-kotlin
+*/
+class OpenAIAPI {
+    // 자신의 API Key
+    private val openAIAPIKey: String = ""
+    // ChatGPT의 응답을 기다리는 시간. 만일 시간이 초과되면 오류가 발생한다.
+    private val duration: Duration = 60.seconds
+    // 모델 ID
+    private val modelID: String = "gpt-3.5-turbo"
+
+    private val openAI: OpenAI = OpenAI(
+        token = openAIAPIKey,
+        timeout = Timeout(socket = duration)
+    )
+
+    /*
+    ChatGPT에게 메시지를 보내고 답변을 받는다.
+
+    [Parameters]
+    - system: ChatGPT의 역할
+    - user: ChatGPT에게 보낼 메시지
+    - maxTokens: 최대 토큰 수
+
+    [Return Value]
+    ChatGPT의 답변을 반환한다. 만일 답변이 없으면 null을 반환한다.
+    */
+    suspend fun chat(system: String, user: String, maxTokens: Int? = null): String? {
+        val chatCompletionRequest = ChatCompletionRequest(
+            model = ModelId(modelID),
+            messages = listOf(
+                ChatMessage(
+                    role = ChatRole.System,
+                    content = system
+                ),
+                ChatMessage(
+                    role = ChatRole.User,
+                    content = user
+                )
+            ),
+            maxTokens = maxTokens
+        )
+
+        val completion: ChatCompletion = openAI.chatCompletion(chatCompletionRequest)
+        return completion.choices[0].message.content
+    }
+}
