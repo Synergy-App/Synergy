ca680094781fccc025b08067b6632bdebb958e4e
heosujinnn <0204sujin@naver.com>
2023-12-31 21:42:23 +0900

    [feat] 교육 공간 Fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
 
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -54,4 +56,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/sungkyul/synergy/LearningFragment.kt
+++ app/src/main/java/com/sungkyul/synergy/LearningFragment.kt
@@ -1,59 +1,94 @@
 package com.sungkyul.synergy
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.charts.HorizontalBarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.sungkyul.synergy.adapter.EduButtonAdapter
+import com.sungkyul.synergy.data.EduButtonItem
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+/** 학습하기 (교육공간) fragment
+ * MPAndroidChart라이브러리 나중에 분리해야할듯 */
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LearningFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LearningFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_learning, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_learning, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LearningFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LearningFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+        val barChart: HorizontalBarChart = view.findViewById(R.id.barChart)
+
+        val myStudyTime = 65f // 예시: 1시간 5분을 분 단위로 변환하여 설정
+        val totalStudyTime = 180f // 전체 값 설정(우선 3시간이 최대라고 설정해둠)
+
+        // 전체 막대 설정
+        val totalEntries = ArrayList<BarEntry>()
+        totalEntries.add(BarEntry(0f, totalStudyTime)) // 전체 막대
+
+        val totalBarDataSet = BarDataSet(totalEntries, "Total Study Time")
+        totalBarDataSet.color = requireContext().getColor(R.color.chatColor) // 투명한 색으로 설정하여 숨김
+
+        // 공부한 시간 막대 설정
+        val studyEntries = ArrayList<BarEntry>()
+        studyEntries.add(BarEntry(0f, myStudyTime)) // 공부한 막대
+
+        val studyBarDataSet = BarDataSet(studyEntries, "My Study Time")
+        studyBarDataSet.color = requireContext().getColor(R.color.learnChatColor) // 공부한 막대 색상 설정
+
+        // 데이터 설정
+        val data = BarData(totalBarDataSet, studyBarDataSet)
+        data.barWidth = 0.1f
+
+        // X축 설정
+        val xAxis = barChart.xAxis
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawAxisLine(false)
+        xAxis.setDrawLabels(false)
+
+        // Y축 설정
+        val leftAxis = barChart.axisLeft
+        leftAxis.setDrawGridLines(false)
+        leftAxis.setDrawLabels(false) // 왼쪽 Y축 텍스트 숨기기
+        leftAxis.axisMinimum = 0f
+
+        val rightAxis = barChart.axisRight
+        rightAxis.setDrawGridLines(false)
+        rightAxis.setDrawAxisLine(false)
+        rightAxis.setDrawLabels(false)
+
+        // 차트 설정
+        barChart.description.isEnabled = false
+        barChart.legend.isEnabled = false
+        barChart.setTouchEnabled(false)
+        barChart.animateY(1000)
+        barChart.data = data
+
+        // RecyclerView 초기화 및 설정
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        val layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = layoutManager
+
+        val buttonItemList = listOf(
+            EduButtonItem("아이콘 교육", Color.parseColor("#FFE7EE")),
+            EduButtonItem("메세지 교육", Color.parseColor("#BAD6FF")),
+            EduButtonItem("전화 교육", Color.parseColor("#BAE0DB")),
+            EduButtonItem("화면구성", Color.parseColor("#C4BAE0")),
+            EduButtonItem("네이버 교육", Color.parseColor("#DFE0BA")),
+            EduButtonItem("카카오톡 교육", Color.parseColor("#E0BAC8")),
+        )
+        val adapter = EduButtonAdapter(buttonItemList)
+        recyclerView.adapter = adapter
+
+        return view
     }
 }
