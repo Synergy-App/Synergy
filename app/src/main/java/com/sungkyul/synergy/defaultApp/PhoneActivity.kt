d57e178eb9db900b9236d6f81523e6fbbb0749fa
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2024-01-31 08:24:59 +0900

    [feat] 프래그먼트 생성 및 하단 뷰 연결, 키패드 프래그먼트 구성

--- app/src/main/java/com/sungkyul/synergy/defaultApp/PhoneActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/defaultApp/PhoneActivity.kt
@@ -2,30 +2,60 @@ package com.sungkyul.synergy.defaultApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.sungkyul.synergy.databinding.ActivityPhoneBinding
 import com.sungkyul.synergy.util.Animation
+import com.sungkyul.synergy.R
 
 class PhoneActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPhoneBinding
+    private lateinit var keypadFragment: Fragment
+    private lateinit var recentHistoryFragment: Fragment
+    private lateinit var contactFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPhoneBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Fragments
+        keypadFragment = KeypadFragment()
+        recentHistoryFragment = RecentHistoryFragment()
+        contactFragment = ContactFragment()
+
+        // 돋보기 버튼에 터치 애니메이션을 추가한다.
         Animation.setViewBgAlphaAnimationOnTouch(
             binding.magnifyingGlassButton,
             0,
             0,
             36,
-            300
+            250
         )
+        // 더보기 버튼에 터치 애니메이션을 추가한다.
         Animation.setViewBgAlphaAnimationOnTouch(
             binding.moreButton,
             0,
             0,
             36,
-            300
+            250
         )
+
+        // 하단 내비게이션 뷰에서 메뉴 아이템을 선택하면, 해당하는 프래그먼트로 교체한다.
+        binding.phoneBottomNav.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.phone_keypad_nav -> replaceFragment(keypadFragment)
+                R.id.phone_recent_history_nav -> replaceFragment(recentHistoryFragment)
+                R.id.phone_contact_nav -> replaceFragment(contactFragment)
+            }
+            true
+        }
+
+        replaceFragment(keypadFragment)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.phoneFragmentContainer.id, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/sungkyul/synergy/defaultApp/RecentHistoryFragment.kt
@@ -0,0 +1,32 @@
+package com.sungkyul.synergy.defaultApp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.sungkyul.synergy.R
+import com.sungkyul.synergy.databinding.ActivityPhoneBinding
+import com.sungkyul.synergy.databinding.FragmentContactBinding
+import com.sungkyul.synergy.databinding.FragmentKeypadBinding
+import com.sungkyul.synergy.databinding.FragmentRecentHistoryBinding
+import com.sungkyul.synergy.util.Animation
+
+private lateinit var binding: FragmentRecentHistoryBinding
+
+class RecentHistoryFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentRecentHistoryBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment.
+        return inflater.inflate(R.layout.fragment_recent_history, container, false)
+    }
+}
