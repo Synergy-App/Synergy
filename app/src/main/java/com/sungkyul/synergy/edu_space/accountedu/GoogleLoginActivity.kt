7be8c4eed2d828f79514e0fc3db74726fc754ac8
dpcdrypak <dpcdrypak@naver.com>
2024-02-08 18:59:39 +0900

    [feat] 계정생성 액티비티, 화면이동 구현

---
+++ app/src/main/java/com/sungkyul/synergy/edu_space/accountedu/GoogleLoginActivity.kt
@@ -0,0 +1,14 @@
+package com.sungkyul.synergy.edu_space.accountedu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sungkyul.synergy.R
+
+class GoogleLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_login)
+
+        // 이곳에 로그인 화면의 구성 요소를 초기화하고 필요한 작업을 수행하세요.
+    }
+}
