00421c423b92a5ae8be8bad4108d32da17cf8494
heosujinnn <0204sujin@naver.com>
2024-02-13 01:28:02 +0900

    [feat] firebase Authentication 로그인 활성화

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation ("com.github.bumptech.glide:glide:4.12.0") // Glide 라이브러리
     implementation ("com.google.android.material:material:1.4.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0") // Glide 어노테이션 프로세서
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/sungkyul/synergy/main/activity/RegisterActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/main/activity/RegisterActivity.kt
@@ -2,11 +2,52 @@ package com.sungkyul.synergy.main.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sungkyul.synergy.R
+import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
+import com.sungkyul.synergy.databinding.ActivityRegisterBinding
+
+/** 파이어베이스 익명 로그인 test*/
+private lateinit var binding: ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var userId: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.synergyRegisterBtn.setOnClickListener {
+            userIdCheck()
+        }
+    }
+
+    private fun userIdCheck() {
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        if (user != null) {
+            userId = user.uid
+            navigateToNextActivity()
+        } else {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        userId = auth.currentUser!!.uid
+                        navigateToNextActivity()
+                    } else {
+                        print("Firebase 로그인 실패!")
+                    }
+                }
+        }
+    }
+
+    private fun navigateToNextActivity() {
+        val intent = Intent(this, DigitalStartActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
