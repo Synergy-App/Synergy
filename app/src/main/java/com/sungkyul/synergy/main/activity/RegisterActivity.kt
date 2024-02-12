e7e41bbc766026610e6bda1f259749427843febf
heosujinnn <0204sujin@naver.com>
2024-02-13 00:25:42 +0900

    [add] 시작 화면 초기 UI

---
+++ app/src/main/java/com/sungkyul/synergy/main/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.sungkyul.synergy.main.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sungkyul.synergy.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
