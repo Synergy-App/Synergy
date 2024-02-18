6462d602147bad0abca2dea20bc0c7eaf1879769
ngl0108 <ngl03032@sungkyul.ac.kr>
2024-02-18 18:30:34 +0900

    payMActivity additon

--- app/src/main/java/com/sungkyul/synergy/edu_space/kakaotaxi/payActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/kakaotaxi/payActivity.kt
@@ -1,7 +1,9 @@
 package com.sungkyul.synergy.edu_space.kakaotaxi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.sungkyul.synergy.R
 
 class payActivity : AppCompatActivity() {
@@ -9,9 +11,16 @@ class payActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
 
+        val Button: Button = findViewById(R.id.button6)
+
+        Button.setOnClickListener {
+            val payMIntent = Intent(this, payMActivity::class.java)
+            startActivity(payMIntent)
+        }
 
 
 
 
     }
 }
+
