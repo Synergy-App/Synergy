d357c1a2cc314ea51661ec0ef724133dbf288009
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2024-02-21 11:35:41 +0900

    [feat] 기본 앱 연결

---
+++ app/src/main/java/com/sungkyul/synergy/edu_space/default_app/activity/DefaultAppActivity.kt
@@ -0,0 +1,74 @@
+package com.sungkyul.synergy.edu_space.default_app.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.sungkyul.synergy.databinding.ActivityDefaultAppBinding
+import com.sungkyul.synergy.edu_space.default_app.TOUCH_DOWN_ALPHA
+import com.sungkyul.synergy.edu_space.default_app.TOUCH_DURATION_ALPHA
+import com.sungkyul.synergy.edu_space.default_app.TOUCH_UP_ALPHA
+import com.sungkyul.synergy.util.AnimUtil
+
+class DefaultAppActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDefaultAppBinding
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDefaultAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 각 버튼의 터치 리스너를 설정한다.
+        binding.callEduBox.setOnTouchListener(onTouchCallEduButtonListener)
+        binding.cameraEduBox.setOnTouchListener(onTouchCameraEduButtonListener)
+        binding.messageEduBox.setOnTouchListener(onTouchMessageEduButtonListener)
+    }
+
+    private val onTouchCallEduButtonListener = View.OnTouchListener { view, event ->
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+            }
+            MotionEvent.ACTION_UP -> {
+                // 전화 앱으로 이동한다.
+                val intent = Intent(applicationContext, DefaultPhoneActivity::class.java)
+                startActivity(intent)
+
+                view.performClick()
+            }
+        }
+        true
+    }
+
+    private val onTouchCameraEduButtonListener = View.OnTouchListener { view, event ->
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+            }
+            MotionEvent.ACTION_UP -> {
+                // 카메라 앱으로 이동한다.
+                val intent = Intent(applicationContext, DefaultCameraActivity::class.java)
+                startActivity(intent)
+
+                view.performClick()
+            }
+        }
+        true
+    }
+
+    private val onTouchMessageEduButtonListener = View.OnTouchListener { view, event ->
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+            }
+            MotionEvent.ACTION_UP -> {
+                // 메시지 앱으로 이동한다.
+                val intent = Intent(applicationContext, DefaultMessageActivity::class.java)
+                startActivity(intent)
+
+                view.performClick()
+            }
+        }
+        true
+    }
+}
