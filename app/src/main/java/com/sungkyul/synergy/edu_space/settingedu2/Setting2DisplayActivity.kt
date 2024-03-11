e6e809ef251695379ff052cfec47bcf0d61535e1
dpcdrypak <dpcdrypak@naver.com>
2024-03-11 17:46:48 +0900

    환경설정2 시크바 밝기조절 수정

--- app/src/main/java/com/sungkyul/synergy/edu_space/settingedu2/Setting2DisplayActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/settingedu2/Setting2DisplayActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.sungkyul.synergy.R
 import com.sungkyul.synergy.edu_space.settingedu2.Setting2FontActivity
@@ -34,5 +35,23 @@ class Setting2DisplayActivity : AppCompatActivity() {
             val intent = Intent(this, Setting2FontActivity::class.java)
             startActivity(intent)
         }
+
+        // 시크바 찾기
+        val brightnessSeekBar = findViewById<SeekBar>(R.id.sb_brightness)
+
+        // 시크바 변경 이벤트 처리
+        brightnessSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                // 시스템 화면 밝기 설정하기 (progress 값으로 밝기 조절)
+                val brightnessValue = progress / 100.0f // SeekBar의 max가 100이므로 비율로 환산
+                val layoutParams = window.attributes
+                layoutParams.screenBrightness = brightnessValue // 화면 밝기 설정
+                window.attributes = layoutParams
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
