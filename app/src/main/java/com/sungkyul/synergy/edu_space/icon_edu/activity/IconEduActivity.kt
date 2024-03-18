b966bbfdaa7ff03cfaf0c0ea3ff79b7aee839b68
ChuminChu <139088490+ChuminChu@users.noreply.github.com>
2024-03-18 10:15:37 +0900

    아이콘 교육

--- app/src/main/java/com/sungkyul/synergy/edu_space/icon_edu/activity/IconEduActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/icon_edu/activity/IconEduActivity.kt
@@ -20,7 +20,7 @@ class IconEduActivity : AppCompatActivity() {
         binding = ActivityIconEduBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*val recyclerView: RecyclerView = binding.iconRv // XML에서 정의한 RecyclerView의 ID를 가져옵니다.
+        val recyclerView: RecyclerView = binding.iconRv // XML에서 정의한 RecyclerView의 ID를 가져옵니다.
 
         // RecyclerView에 레이아웃 매니저 설정
         recyclerView.layoutManager = LinearLayoutManager(this) // LinearLayoutManager 또는 필요한 레이아웃 매니저를 사용합니다.
@@ -28,11 +28,11 @@ class IconEduActivity : AppCompatActivity() {
 
             // 데이터를 생성하거나 가져와서 리스트를 만듭니다. 여기서는 예시로 더미 데이터를 만듭니다.
         val iconList = arrayListOf(
-            Icon(R.drawable.ic_sound_black, "음량 조절"),
-            Icon(R.drawable.ic_sound_black, "와이파이"),
+            Icon(R.drawable.ic_wifi_black, "와이파이"),
+            Icon(R.drawable.ic_sound_black, "소리(음량)"),
             Icon(R.drawable.ic_sound_black, "손전등"),
-            Icon(R.drawable.ic_sound_black, "블루투스"),
-            Icon(R.drawable.ic_sound_black, "비행기모드"),
+            Icon(R.drawable.ic_bluetooth_black_91, "블루투스"),
+            Icon(R.drawable.ic_sound_black, "배터리"),
             Icon(R.drawable.ic_sound_black, "절전모드"),
             Icon(R.drawable.ic_sound_black, "데이터 모바일"),
             Icon(R.drawable.ic_sound_black, "화면 녹화"),
@@ -42,6 +42,6 @@ class IconEduActivity : AppCompatActivity() {
         )
         // RecyclerView Adapter 생성 및 연결
         iconAdapter = IconEduAdapter(this, iconList) // 여기에 생성한 아이템 리스트를 넣어줍니다.
-        recyclerView.adapter = iconAdapter*/
+        recyclerView.adapter = iconAdapter
     }
 }
