cfce0173e5e6e9056dfa558a0ef79031de899fc7
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2024-03-19 19:09:57 +0900

    [feat] 교육이 없는 현재 모든 화면에 EduScreen 추가

--- app/src/main/java/com/sungkyul/synergy/edu_space/delivery/DlvMainActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/delivery/DlvMainActivity.kt
@@ -7,14 +7,50 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.SearchView
+import androidx.activity.OnBackPressedCallback
+import com.sungkyul.synergy.MainActivity
 import com.sungkyul.synergy.R
+import com.sungkyul.synergy.databinding.ActivityDlvMainBinding
 import com.sungkyul.synergy.edu_space.delivery.shopActivity
+import com.sungkyul.synergy.utils.edu.EduCourses
 
 class DlvMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDlvMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dlv_main)
+        binding = ActivityDlvMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // 교육을 정의해보자!
+        binding.eduScreen.post {
+            // 교육 코스 customCourse를 지정한다.
+            binding.eduScreen.course = EduCourses.dlvMainCourse(
+                binding.eduScreen.context,
+                binding.eduScreen.width.toFloat(),
+                binding.eduScreen.height.toFloat()
+            )
+            binding.eduScreen.setOnFinishedCourseListener {
+                // 교육 코스가 끝났을 때 어떻게 할지 처리하는 곳이다.
+
+                // MainActivity로 되돌아 간다.
+                val intent = Intent(binding.root.context, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+            }
+            // 교육을 시작한다.
+            binding.eduScreen.start(this)
+        }
+
+        // 뒤로 가기 키를 눌렀을 때의 이벤트를 처리한다.
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // MainActivity로 되돌아 간다.
+                val intent = Intent(binding.root.context, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+            }
+        })
 
         //검색창에 목적지 입력시 가게목록으로 넘어감(검색어 intent로 전달)
         val searchView: SearchView = findViewById(R.id.searchView2)
--- app/src/main/java/com/sungkyul/synergy/edu_space/ticket/Ticket2MainActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/ticket/Ticket2MainActivity.kt
@@ -4,13 +4,49 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import com.sungkyul.synergy.MainActivity
 import com.sungkyul.synergy.R
+import com.sungkyul.synergy.databinding.ActivityTicket2MainBinding
+import com.sungkyul.synergy.utils.edu.EduCourses
 
 class Ticket2MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTicket2MainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityTicket2MainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_ticket2_main)
 
+        // 교육을 정의해보자!
+        binding.eduScreen.post {
+            // 교육 코스 customCourse를 지정한다.
+            binding.eduScreen.course = EduCourses.ticket2MainCourse(
+                binding.eduScreen.context,
+                binding.eduScreen.width.toFloat(),
+                binding.eduScreen.height.toFloat()
+            )
+            binding.eduScreen.setOnFinishedCourseListener {
+                // 교육 코스가 끝났을 때 어떻게 할지 처리하는 곳이다.
+
+                // MainActivity로 되돌아 간다.
+                val intent = Intent(binding.root.context, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+            }
+            // 교육을 시작한다.
+            binding.eduScreen.start(this)
+        }
+
+        // 뒤로 가기 키를 눌렀을 때의 이벤트를 처리한다.
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // MainActivity로 되돌아 간다.
+                val intent = Intent(binding.root.context, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+            }
+        })
 
         val resButton1: Button = findViewById(R.id.button16)
 
@@ -18,9 +54,5 @@ class Ticket2MainActivity : AppCompatActivity() {
             val mapIntent = Intent(this, resActivity::class.java)
             startActivity(mapIntent)
         }
-
     }
-
-
-
 }
