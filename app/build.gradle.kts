888e9a36270ef05619003fa1d251ec32ba3ea56e
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2024-05-01 13:07:02 +0900

    ⬆️ 의존성 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,10 +56,11 @@ android {
 }
 
 dependencies {
-    implementation ("com.squareup.retrofit2:converter-gson:2.7.1")
     // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+
     // OkHttp
     implementation ("com.squareup.okhttp3:okhttp:4.10.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
