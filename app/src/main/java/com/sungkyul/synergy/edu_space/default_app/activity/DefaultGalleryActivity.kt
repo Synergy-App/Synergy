45f474a63909b2d7835b9d0864b92d96badf0f16
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2024-05-02 18:29:08 +0900

    ✨ 갤러리 액티비티에 앨범 프래그먼트 추가

--- app/src/main/java/com/sungkyul/synergy/edu_space/default_app/activity/DefaultGalleryActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/default_app/activity/DefaultGalleryActivity.kt
@@ -2,14 +2,27 @@ package com.sungkyul.synergy.edu_space.default_app.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.sungkyul.synergy.databinding.ActivityDefaultGalleryBinding
+import com.sungkyul.synergy.edu_space.default_app.fragment.DefaultGalleryAlbumFragment
 
 class DefaultGalleryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDefaultGalleryBinding
+    private lateinit var albumFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDefaultGalleryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        albumFragment = DefaultGalleryAlbumFragment(binding.eduScreen)
+
+        replaceFragment(albumFragment)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.container.id, fragment)
+            .commit()
     }
 }
