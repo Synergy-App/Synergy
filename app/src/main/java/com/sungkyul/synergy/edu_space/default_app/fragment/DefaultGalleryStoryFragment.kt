22cf56ebeea47cca39c97c4f2a3ede4ebfa2fc52
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2024-05-03 22:00:15 +0900

    ✨(갤러리/스토리) 리스트 구현

--- app/src/main/java/com/sungkyul/synergy/edu_space/default_app/fragment/DefaultGalleryStoryFragment.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/default_app/fragment/DefaultGalleryStoryFragment.kt
@@ -10,7 +10,8 @@ import com.sungkyul.synergy.R
 import com.sungkyul.synergy.databinding.FragmentDefaultGalleryAlbumBinding
 import com.sungkyul.synergy.databinding.FragmentDefaultGalleryStoryBinding
 import com.sungkyul.synergy.edu_space.default_app.adapter.GalleryAlbumAdapter
-import com.sungkyul.synergy.edu_space.default_app.adapter.GalleryAlbumData
+import com.sungkyul.synergy.edu_space.default_app.adapter.GalleryStoryAdapter
+import com.sungkyul.synergy.edu_space.default_app.adapter.GalleryStoryData
 import com.sungkyul.synergy.utils.edu.EduListener
 
 class DefaultGalleryStoryFragment(private val eduListener: EduListener) : Fragment() {
@@ -21,6 +22,20 @@ class DefaultGalleryStoryFragment(private val eduListener: EduListener) : Fragme
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentDefaultGalleryStoryBinding.inflate(inflater, container, false)
+
+        val dataSet = ArrayList<GalleryStoryData>()
+        for(i in 1..10) {
+            dataSet.add(
+                GalleryStoryData(
+                    R.drawable.todo_rect,
+                    "2028-05-01"
+                )
+            )
+        }
+
+        val recyclerview = binding.recyclerview
+        recyclerview.layoutManager = GridLayoutManager(binding.root.context, 2)
+        recyclerview.adapter = GalleryStoryAdapter(dataSet)
 
         return binding.root
     }
