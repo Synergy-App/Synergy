6005a79fde3d0880dd623cbd653516438c7beb22
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2024-05-05 20:09:26 +0900

    강제 pull

--- app/src/main/java/com/sungkyul/synergy/edu_space/default_app/activity/DefaultPhoneAddActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/default_app/activity/DefaultPhoneAddActivity.kt
@@ -1,15 +1,11 @@
 package com.sungkyul.synergy.edu_space.default_app.activity
 
 import DefaultPhoneContactFragment
-import android.content.Intent
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.sungkyul.synergy.R
 import com.sungkyul.synergy.databinding.ActivityDefaultPhoneAddBinding
-import com.sungkyul.synergy.edu_space.default_app.adapter.ContactData
 import com.sungkyul.synergy.utils.AnimUtils
 
 class DefaultPhoneAddActivity : AppCompatActivity() {
