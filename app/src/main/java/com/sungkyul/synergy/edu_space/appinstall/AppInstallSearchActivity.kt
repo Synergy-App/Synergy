196907e96a6df7cd9734e715401df7d849e63bff
dpcdrypak <dpcdrypak@naver.com>
2024-05-09 16:50:13 +0900

    앱설치 구현중

---
+++ app/src/main/java/com/sungkyul/synergy/edu_space/appinstall/AppInstallSearchActivity.kt
@@ -0,0 +1,14 @@
+package com.sungkyul.synergy.edu_space.appinstall
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sungkyul.synergy.R
+
+class AppInstallSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appinstall_search)
+
+        // 이곳에 액티비티의 초기화 작업을 추가하세요
+    }
+}
