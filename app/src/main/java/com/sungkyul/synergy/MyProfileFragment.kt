f9e7e917a4382572f40aa8c580cf3dc1217aede3
dpcdrypak <dpcdrypak@naver.com>
2024-05-12 20:40:50 +0900

    앱설치 끝

--- app/src/main/java/com/sungkyul/synergy/MyProfileFragment.kt
+++ app/src/main/java/com/sungkyul/synergy/MyProfileFragment.kt
@@ -4,7 +4,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.sungkyul.synergy.R
 import com.sungkyul.synergy.databinding.FragmentMyProfileBinding // Import your binding class
 
