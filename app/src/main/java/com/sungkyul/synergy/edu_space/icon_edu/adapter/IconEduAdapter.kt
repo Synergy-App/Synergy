fdb411292ebdc6c846368e94648d4dafb5bab317
ChuminChu <139088490+ChuminChu@users.noreply.github.com>
2024-06-04 21:20:41 +0900

    textview

--- app/src/main/java/com/sungkyul/synergy/edu_space/icon_edu/adapter/IconEduAdapter.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/icon_edu/adapter/IconEduAdapter.kt
@@ -15,8 +15,11 @@ import com.sungkyul.synergy.edu_space.icon_edu.activity.IconDetailActivity
 import com.sungkyul.synergy.utils.GalaxyButton
 
 /** 교육공간 속 아이콘 리사이클러뷰 어뎁터 */
-class IconEduAdapter(private val context: Context, private val iconList: ArrayList<Icon>) :
-    RecyclerView.Adapter<IconEduAdapter.CustomViewHolder>() {
+class IconEduAdapter(
+    private val context: Context,
+    private val iconList: ArrayList<Icon>,
+    private val standardSize_X: Int // 추가된 부분
+) : RecyclerView.Adapter<IconEduAdapter.CustomViewHolder>() {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -24,7 +27,7 @@ class IconEduAdapter(private val context: Context, private val iconList: ArrayLi
     ): CustomViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = FragmentIconListBinding.inflate(inflater, parent, false)
-        return CustomViewHolder(binding, )
+        return CustomViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
@@ -111,43 +114,53 @@ class IconEduAdapter(private val context: Context, private val iconList: ArrayLi
                                 R.drawable.ic_word_volume,
                                 "볼륨",
                                 "소리의 크기를 조절하는 것"
-                            )"셀카" -> IconInfo(
+                            )
+                            "셀카" -> IconInfo(
                                 R.drawable.ic_wifi_black,
                                 "셀카",
                                 "카메라로 자신의 얼굴을 스스로 찍는 사진"
-                            )"메일(이메일)" -> IconInfo(
+                            )
+                            "메일(이메일)" -> IconInfo(
                                 R.drawable.ic_wifi_black,
                                 "메일(이메일)",
                                 "인터넷을 통해 편지를 보내고 받는 것 <hr><hr> 예를 들어, 예전에 우편으로 편지를 보내듯이 이제는 컴퓨터나 휴대폰을 통해 메일을 보내고 받음"
-                            )"이메일 주소" -> IconInfo(
+                            )
+                            "이메일 주소" -> IconInfo(
                                 R.drawable.ic_wifi_black,
                                 "이메일 주소",
                                 "인터넷에서 메일을 주고받을 때 쓰는 주소<hr> '아이디@naver.com'와 같은 형태"
-                            )"프로필" -> IconInfo(
+                            )
+                            "프로필" -> IconInfo(
                                 R.drawable.ic_word_profile,
                                 "프로필",
                                 "이름이나 사진 등을 넣어 자신을 소개하는 공간"
-                            )"상태 메세지" -> IconInfo(
+                            )
+                            "상태 메세지" -> IconInfo(
                                 R.drawable.ic_wifi_black,
                                 "상태 메세지",
                                 "짧게 자신의 감정이나 상태를 말하는 메세지"
-                            )"폴더" -> IconInfo(
+                            )
+                            "폴더" -> IconInfo(
                                 R.drawable.ic_word_folder,
                                 "폴더",
                                 "파일들을 정리하고 모아놓는 곳 <hr><hr> 앨범에서 저장된 경로에 따라 사진이 분류되는 것도 폴더로 분류된다고 함"
-                            )"맵(지도)" -> IconInfo(
+                            )
+                            "맵(지도)" -> IconInfo(
                                 R.drawable.ic_word_map,
                                 "맵(지도)",
                                 "길을 찾거나 위치를 확인할 때 사용하는 지도"
-                            )"다운로드(설치)" -> IconInfo(
+                            )
+                            "다운로드(설치)" -> IconInfo(
                                 R.drawable.ic_word_download,
                                 "다운로드(설치)",
                                 "이넡넷으로 자료나 앱을 휴대폰으로 가져오는 것"
-                            )"채팅" -> IconInfo(
+                            )
+                            "채팅" -> IconInfo(
                                 R.drawable.ic_word_chatting,
                                 "채팅",
                                 "문자를 주고 받으며 소통하는 것"
-                            )"키보드" -> IconInfo(
+                            )
+                            "키보드" -> IconInfo(
                                 R.drawable.ic_wifi_black,
                                 "키보드",
                                 "자판 입력 장치로 휴대폰에서 글자나 기호를 입력할 수 있는 창"
@@ -156,23 +169,28 @@ class IconEduAdapter(private val context: Context, private val iconList: ArrayLi
                                 R.drawable.ic_wifi_black,
                                 "업데이트",
                                 "기존의 것을 최신으로 수정, 추가, 보안하는 것"
-                            )"앱(어플)" -> IconInfo(
+                            )
+                            "앱(어플)" -> IconInfo(
                                 R.drawable.ic_wifi_black,
                                 "앱(어플)",
                                 "어플리케이션의 줄임말<hr>카메라, 문자와 같이 다영한 작업을 할 수 있음"
-                            )"모바일뱅킹" -> IconInfo(
+                            )
+                            "모바일뱅킹" -> IconInfo(
                                 R.drawable.ic_wifi_black,
                                 "모바일 뱅킹",
                                 "휴대폰에서 은행 업무를 볼 수 있는 기능"
-                            )"업로드" -> IconInfo(
+                            )
+                            "업로드" -> IconInfo(
                                 R.drawable.ic_word_upload,
                                 "업로드",
                                 "자료를 전송하는 일<hr>특정 자료를 '올리다'와 같은 맥락으로 사용"
-                            )"푸쉬 알림" -> IconInfo(
+                            )
+                            "푸쉬 알림" -> IconInfo(
                                 R.drawable.ic_word_alarm,
                                 "푸쉬 알림",
                                 "앱에서 보내는 실시간 알림"
-                            )"음성 메세지" -> IconInfo(
+                            )
+                            "음성 메세지" -> IconInfo(
                                 R.drawable.ic_wifi_black,
                                 "음성 메세지",
                                 "통화 기능과는 별개로 짧은 음성 정보를 보낼 수 있는 기능"
@@ -200,7 +218,9 @@ class IconEduAdapter(private val context: Context, private val iconList: ArrayLi
                 iconIv.setImageResource(icon.iconImage)
                 iconTv.text = icon.iconText
 
-                }
+                // 텍스트 크기 설정
+                iconTv.textSize = (standardSize_X / 20).toFloat()
             }
         }
     }
+}
