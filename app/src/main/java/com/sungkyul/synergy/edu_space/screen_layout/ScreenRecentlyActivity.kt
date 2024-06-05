472f2ccb4b71506b78e159fe1ca84e7f958e5a61
dpcdrypak <dpcdrypak@naver.com>
2024-06-05 18:27:52 +0900

    화면구성 화면 재제작

--- app/src/main/java/com/sungkyul/synergy/edu_space/screen_layout/ScreenRecentlyActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/edu_space/screen_layout/ScreenRecentlyActivity.kt
@@ -1,6 +1,11 @@
 package com.sungkyul.synergy.edu_space.screen_layout
 
 import android.os.Bundle
+import android.view.View
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.UnderlineSpan
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.sungkyul.synergy.R
 
@@ -8,5 +13,19 @@ class ScreenRecentlyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen_recently)
+
+        // 하단바 숨기기 설정
+        hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                )
     }
 }
