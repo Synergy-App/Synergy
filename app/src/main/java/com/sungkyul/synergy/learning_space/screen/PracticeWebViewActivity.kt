59181bca21d9a62ece0c5fda9b581a9120c1f746
heosujinnn <0204sujin@naver.com>
2024-06-08 03:27:14 +0900

    [refactor] figma

--- app/src/main/java/com/sungkyul/synergy/learning_space/screen/PracticeWebViewActivity.kt
+++ app/src/main/java/com/sungkyul/synergy/learning_space/screen/PracticeWebViewActivity.kt
@@ -1,21 +1,45 @@
 package com.sungkyul.synergy.learning_space.screen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.webkit.WebView
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.sungkyul.synergy.MainActivity
 import com.sungkyul.synergy.R
+import com.sungkyul.synergy.databinding.ActivityPracticeWebViewBinding
 
 class PracticeWebViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPracticeWebViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_practice_web_view)
+        binding = ActivityPracticeWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val webView: WebView = findViewById(R.id.webView)
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://www.youtube.com/?hl=ko&gl=KR&app=desktop")
 
-        // JavaScript 활성화
-        webView.settings.javaScriptEnabled = true
+        // Include된 레이아웃에서 버튼 찾기
+        val includeLayout = findViewById<RelativeLayout>(R.id.practice_nav_layout)
 
-        // 웹 페이지 로드
-        webView.loadUrl("https://www.youtube.com/?hl=ko&gl=KR&app=desktop")
+        val button1 = includeLayout.findViewById<ImageView>(R.id.home_nav)
+        val button2 = includeLayout.findViewById<ImageView>(R.id.back_nav)
+        val button3 = includeLayout.findViewById<ImageView>(R.id.next_nav)
+
+        button1.setOnClickListener {
+            val intent = Intent(this@PracticeWebViewActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button2.setOnClickListener {
+            // val intent = Intent(this@PracticeWebViewActivity, MainActivity::class.java)
+            // startActivity(intent)
+        }
+
+        button3.setOnClickListener {
+            // val intent = Intent(this@PracticeWebViewActivity, NewActivity3::class.java)
+            // startActivity(intent)
+        }
     }
 }
