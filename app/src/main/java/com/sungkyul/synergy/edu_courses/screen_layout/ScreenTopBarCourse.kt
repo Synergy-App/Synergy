fb0e94c3a42b5cdade1cda8dc6a787a4b8bec62e
SweetHoneyNougat <sky1357@sungkyul.ac.kr>
2024-06-11 05:29:16 +0900

    코스 클래스 추가

---
+++ app/src/main/java/com/sungkyul/synergy/edu_courses/screen_layout/ScreenTopBarCourse.kt
@@ -0,0 +1,18 @@
+package com.sungkyul.synergy.com.sungkyul.synergy.edu_courses.screen_layout
+
+import com.sungkyul.synergy.utils.DisplayUtils
+import com.sungkyul.synergy.utils.edu.EduCourse
+import com.sungkyul.synergy.utils.edu.EduData
+import com.sungkyul.synergy.utils.edu.EduScreen
+
+data class ScreenTopBarCourse(val eduScreen: EduScreen): EduCourse {
+    override val list = ArrayList<EduData>()
+    override val width = DisplayUtils.pxToDp(eduScreen.context, eduScreen.width.toFloat())
+    override val height = DisplayUtils.pxToDp(eduScreen.context, eduScreen.height.toFloat())
+
+    // 교육 코스를 만든다.
+    init {
+        list.add(EduData().apply {
+        })
+    }
+}
