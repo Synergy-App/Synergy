56580f3b5bddc0e1302565c4730fa86e9bd6d5ed
heosujinnn <0204sujin@naver.com>
2024-06-11 03:37:25 +0900

    feat : modify fragment

--- app/src/main/java/com/sungkyul/synergy/adapter/EduButtonAdapter.kt
+++ app/src/main/java/com/sungkyul/synergy/adapter/EduButtonAdapter.kt
@@ -8,21 +8,21 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sungkyul.synergy.R
+import com.sungkyul.synergy.databinding.ActivityDefaultAppBinding
 import com.sungkyul.synergy.edu_space.accountedu.GoogleMainActivity
 import com.sungkyul.synergy.edu_space.appinstall.AppInstallMainActivity
-import com.sungkyul.synergy.edu_space.default_app.DefaultAppActivity
-import com.sungkyul.synergy.edu_space.delivery.DlvMainActivity
 import com.sungkyul.synergy.edu_space.icon_edu.activity.BasicEduMainFragment
 import com.sungkyul.synergy.edu_space.kakaotalk.activity.KakaoMainActivity
-import com.sungkyul.synergy.edu_space.kakaotaxi.TaxiMainActivity
 import com.sungkyul.synergy.edu_space.naver.activity.NaverActivity
 import com.sungkyul.synergy.edu_space.screen_layout.ScreenLayoutActivity
 import com.sungkyul.synergy.edu_space.settingedu2.Setting2MainActivity
-import com.sungkyul.synergy.edu_space.ticket.TicketMainActivity
 import com.sungkyul.synergy.utils.GalaxyButton
 
 data class EduButtonItem(
@@ -41,6 +41,15 @@ class EduButtonAdapter(
             .inflate(R.layout.learning_item_button, parent, false)
         return ButtonViewHolder(view)
     }
+    private var onItemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.onItemClickListener = listener
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
 
     override fun onBindViewHolder(holder: ButtonViewHolder, position: Int) {
         holder.bind(buttonList[position])
@@ -52,11 +61,14 @@ class EduButtonAdapter(
 
     @SuppressLint("ClickableViewAccessibility")
     inner class ButtonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val title: TextView = itemView.findViewById(R.id.title)
-        private val icon: ImageView = itemView.findViewById(R.id.edu_icon)
+        private val layout1: LinearLayout = itemView.findViewById(R.id.learning_layout)
+        private val text1: TextView = itemView.findViewById(R.id.title)
+        private val text2: TextView = itemView.findViewById(R.id.learning_tv2)
+        private val imageView: ImageView = itemView.findViewById(R.id.edu_icon)
         private val eduButton: GalaxyButton = itemView.findViewById(R.id.edu_button)
 
         init {
+            text2.text = "교육"
             eduButton.post {
                 eduButton.clipToRoundRect(27.0f)
             }
@@ -66,61 +78,26 @@ class EduButtonAdapter(
                     MotionEvent.ACTION_DOWN -> {
                         (view as GalaxyButton).startTouchDownAnimation(event.x, event.y, 100.0f)
                     }
-
                     MotionEvent.ACTION_UP -> {
                         (view as GalaxyButton).startTouchUpAnimation()
 
-                        when (title.text.toString()) {
-                            "기초" -> {
-                                val fragment = BasicEduMainFragment()
-                                fragmentManager.beginTransaction()
-                                    .replace(R.id.fragment_container_view, fragment)
-                                    .addToBackStack(null)
-                                    .commit()
-                            }
-                            "화면구성" -> {
-                                val intent = Intent(context, ScreenLayoutActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "기본 앱" -> {
-                                val intent = Intent(context, DefaultAppActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "환경 설정" -> {
-                                val intent = Intent(context, Setting2MainActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "계정 생성" -> {
-                                val intent = Intent(context, GoogleMainActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "앱 설치" -> {
-                                val intent = Intent(context, AppInstallMainActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "카카오톡" -> {
-                                val intent = Intent(context, KakaoMainActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "네이버" -> {
-                                val intent = Intent(context, NaverActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "코레일" -> {
-                                val intent = Intent(context, TicketMainActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "카카오택시" -> {
-                                val intent = Intent(context, TaxiMainActivity::class.java)
-                                context.startActivity(intent)
-                            }
-                            "배달의 민족" -> {
-                                val intent = Intent(context, DlvMainActivity::class.java)
-                                context.startActivity(intent)
-                            }
+                        val context = itemView.context
+                        val activity = context as FragmentActivity
+                        val title = text1.text.toString()
+                        when (title) {
+                            // Intent로 Activity 시작
+                            "화면구성" -> context.startActivity(Intent(context, ScreenLayoutActivity::class.java))
+                            "환경 설정" -> context.startActivity(Intent(context, Setting2MainActivity::class.java))
+                            "기본 앱" -> context.startActivity(Intent(context, ActivityDefaultAppBinding::class.java))
+                            "계정 생성" -> context.startActivity(Intent(context, GoogleMainActivity::class.java))
+                            "앱 설치" -> context.startActivity(Intent(context, AppInstallMainActivity::class.java))
+                            "카카오톡" -> context.startActivity(Intent(context, KakaoMainActivity::class.java))
+                            "네이버" -> context.startActivity(Intent(context, NaverActivity::class.java))
+                            // Fragment 시작
+                            "기초" -> addFragment(activity, BasicEduMainFragment())
+                            else -> null
                         }
                     }
-
                     MotionEvent.ACTION_CANCEL -> {
                         (view as GalaxyButton).startTouchUpAnimation()
                     }
@@ -129,9 +106,17 @@ class EduButtonAdapter(
             }
         }
 
+        private fun addFragment(activity: FragmentActivity, fragment: Fragment) {
+            val fragmentManager = activity.supportFragmentManager
+            val transaction = fragmentManager.beginTransaction()
+            transaction.add(R.id.mainMainFrameLayout, fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
         fun bind(buttonItem: EduButtonItem) {
-            title.text = buttonItem.buttonText
-            icon.setImageResource(buttonItem.imageResId)
+            text1.text = buttonItem.buttonText
+            imageView.setImageResource(buttonItem.imageResId)
         }
     }
 }
